2991537b3b36224ea071635cb018796221be0247
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-08-09 00:18:54 +0200

    feat: Hallo welt fix

--- icworld/app/page.tsx
+++ icworld/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-	return
-	(
-		<div>
-			<d className="bg-gray-800 m-5">Hallo Welt</d>
-			<d className="bg-gray-800 m-5">Hallo Welt</d>
+	return(
+		<div className="m-4">
+			Hallo World
 		</div>
 	)
 }
