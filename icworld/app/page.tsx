1099e88a074c3d4baea4c0da78b1839255fcfc83
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-08-09 18:47:17 +0200

    feat: rm unnessery

--- icworld/app/page.tsx
+++ icworld/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import InputField from "@/components/inputfild";
 import ResponsiveText from "@/components/responsiveText";
 
@@ -10,25 +9,12 @@ export default function Home() {
 
 	return(
 		<div>
+			<div className="p-5"></div>
 			<ResponsiveText title="IC World"/>
+			<div className="p-5"></div>
 			<InputField placeholder="Search for an IC Nr." onEnter={handleSearch}/>
 
-			<div className="flex">
-				<div className="flex-1">
-					<ResponsiveText title="Left Text" />
-					<div className="p-5"></div>
-					<ResponsiveText title="Left Text" />
-					<div className="p-5"></div>
-					<div className="flex">
-						<ResponsiveText title="Left Text" />
-						<ResponsiveText title="Left Text" />
-						<ResponsiveText title="Left Text" />
-					</div>
-				</div>
-				<ResponsiveText title="midle Text" />
-				<ResponsiveText title="Right Text" />
-				
-			</div>
+			<div className="p-5"></div>
 
 		</div>
 	)
