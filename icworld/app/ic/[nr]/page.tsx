ad25ed6c20fba38d6f07b7249f9c5dfd661b3d9f
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-08-10 11:53:55 +0200

    fix: build fix

--- icworld/app/ic/[nr]/page.tsx
+++ icworld/app/ic/[nr]/page.tsx
@@ -2,19 +2,22 @@ import ResponsiveText from "@/components/responsiveText";
 import { getIcNameByNr, getIcPinoutByNr, getIcVidByNr } from "@/lib/db/ics";
 import AskiArt from "@/components/askiArt";
 import VideoEmbed from "@/components/videoEmbed";
-type Props = {
-  params: { nr: string };
+
+type PageProps = {
+  params: Promise<{ nr: string }>;
 };
 
-export default async function IcPage({ params }: Props) {
-  const name = await getIcNameByNr(params.nr);
-  const pinout = await getIcPinoutByNr(params.nr);
-  const vid = await getIcVidByNr(params.nr);
+export default async function IcPage({ params }: PageProps) {
+  const { nr } = await params;
+
+  const name = await getIcNameByNr(nr);
+  const pinout = await getIcPinoutByNr(nr);
+  const vid = await getIcVidByNr(nr);
 
   return (
     <div>
       <div className="p-5"></div>
-      <ResponsiveText text={`${name} | ${params.nr}`} />
+      <ResponsiveText text={`${name} | ${nr}`} />
       <div className="p-5"></div>
       <AskiArt asciiArt={pinout} />
       <div className="p-10">
